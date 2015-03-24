e8960bcfd21fd355b960130c831c02cc5809ea29
Hallvard Traetteberg <hal@idi.ntnu.no>
2015-03-24 17:28:02 +0100

    Glemte å commit-e, se forrige commit.

--- no.hal.eclipsky.services.emfs/src/no/hal/eclipsky/services/emfs/EmfsService.java
+++ no.hal.eclipsky.services.emfs/src/no/hal/eclipsky/services/emfs/EmfsService.java
@@ -3,10 +3,11 @@ package no.hal.eclipsky.services.emfs;
 import java.util.Collection;
 
 import no.hal.emfs.EmfsResource;
+import no.hal.emfs.util.ImportHelperOptions;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public interface EmfsService {
 
-	public void importResources(Collection<EmfsResource> emfsResources, String projectName, boolean overwrite, IProgressMonitor monitor) throws Exception;
+	public void importResources(Collection<EmfsResource> emfsResources, String projectName, ImportHelperOptions options, IProgressMonitor monitor) throws Exception;
 }
