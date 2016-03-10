0654046563b4508cdd7f4ac62caf711eaa9b268c
Hallvard Traetteberg <hal@idi.ntnu.no>
2016-03-10 17:46:46 +0100

    Koblet bruk av websockets ordentlig ut.

--- no.hal.eclipsky.services.workspace.http/web/js/connector.js
+++ no.hal.eclipsky.services.workspace.http/web/js/connector.js
@@ -79,38 +79,35 @@ var connector = (function() {
 		link.href = url;
 		link.protocol = "ws";
 	    webSocket = new WebSocket(link.href);
-	    if (webSocket.readyState == WebSocket.CLOSING || webSocket.readyState == WebSocket.CLOSED) {
-	    	webSocket = null;
-	    } else {
-	        connectionClosed = false;
+	    
+        connectionClosed = false;
 
-		    webSocket.onerror = function(event) {
-				// TODO: Reconnect?
-		    	c.log('ws error', event);
-		    	// publish(event);
-		    };
-	
-		    webSocket.onclose = function(event) {
-		    	connectionClosed = true;
-		        c.log('ws close', event);
-	
-		        var response = {type : 'connectionClosed', event: event};
-		        publish(response);
-		    };
-	
-		    webSocket.onmessage = function(event) {
-				pop();
-				var data = JSON.parse(event.data);
-		    	publish(data);
-		    }
-		}
+	    webSocket.onerror = function(event) {
+	    	webSocket = null;
+	    	c.log('ws error', event);
+	    	// publish(event);
+	    };
+
+	    webSocket.onclose = function(event) {
+	    	connectionClosed = true;
+	        c.log('ws close', event);
+
+	        var response = {type : 'connectionClosed', event: event};
+	        publish(response);
+	    };
+
+	    webSocket.onmessage = function(event) {
+			pop();
+			var data = JSON.parse(event.data);
+	    	publish(data);
+	    }
 	};
-	
 	
 	return {
 		init : function(config) {
 			url = config.url;
-			if (window.WebSocket) {
+			// remove websocket support for now
+			if (false && window.WebSocket) {
 				initializeWebsocket(url);
 			}
 		},
