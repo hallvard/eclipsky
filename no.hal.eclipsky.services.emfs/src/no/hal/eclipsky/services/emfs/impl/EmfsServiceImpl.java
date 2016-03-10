8ed860c386a8d24351462d187e18395bd6ee3f6a
Hallvard Traetteberg <hal@idi.ntnu.no>
2016-03-10 17:44:18 +0100

    Bruker lyttere for å registrere eksporterte filer.

--- no.hal.eclipsky.services.emfs/src/no/hal/eclipsky/services/emfs/impl/EmfsServiceImpl.java
+++ no.hal.eclipsky.services.emfs/src/no/hal/eclipsky/services/emfs/impl/EmfsServiceImpl.java
@@ -1,8 +1,10 @@
 package no.hal.eclipsky.services.emfs.impl;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.osgi.service.component.annotations.Component;
@@ -18,6 +20,7 @@ import no.hal.emfs.sync.ExportRule;
 import no.hal.emfs.sync.ExportSpec;
 import no.hal.emfs.sync.RelativePath;
 import no.hal.emfs.sync.SyncFactory;
+import no.hal.emfs.util.AbstractPorter.Listener;
 import no.hal.emfs.util.CompositeExportSupport;
 import no.hal.emfs.xtext.XemfsStandaloneSetup;
 
@@ -53,8 +56,17 @@ public class EmfsServiceImpl implements EmfsService {
 		exportSpec.getRules().add(rule);
 		Exporter exporter = new Exporter(exportSpec);
 		exporter.setRootContainer(project);
+		final Collection<EmfsResource> exportedResources = new ArrayList<EmfsResource>();
+		exporter.addListener(new Listener() {
+			@Override
+			public void resourceHandled(EmfsResource emfsResource, IResource resource, int what) {
+				if ((what & Listener.EXPORTED) > 0) {
+					exportedResources.add(emfsResource);
+				}
+			}
+		});
 		exporter.exportResources(emfsResources, monitor);
-		return emfsResources;
+		return exportedResources;
 	}
 
 	private RelativePath createRulePath(String pathString) {
