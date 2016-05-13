3c8c2601cac948ccf5c42de7fe36393e1e8c2070
Hallvard Traetteberg <hal@idi.ntnu.no>
2016-05-13 10:27:53 +0200

    More merges after git problem...

--- no.hal.eclipsky.services.workspace.http/src/no/hal/eclipsky/services/workspace/http/WorkspaceServiceServlet.java
+++
@@ -1,7 +0,0 @@
-package no.hal.eclipsky.services.workspace.http;
-
-import no.hal.eclipsky.services.workspace.WorkspaceService;
-
-public interface WorkspaceServiceServlet extends ServiceServlet {
-	public void setWorkspaceService(WorkspaceService workspaceService);
-}
